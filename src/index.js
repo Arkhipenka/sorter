b55ff52f762ddf15c6d516d11756566c5750d658
Arkhipenka <europeoplebelarus@gmail.com>
2018-02-23 21:35:43 +0300

    100%

--- src/index.js
+++ src/index.js
@@ -1,30 +1,46 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.Array = new Array() 
+    this.compare = function(a, b){
+      if(a > b) return 1;
+      if(a < b) return -1;
+      if(a == b) return 0;
+    }
   }
 
   add(element) {
-    // your implementation
+    this.Array.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.Array[index];
   }
 
   get length() {
-    // your implementation
+    return this.Array.length;
   }
 
   toArray() {
-    // your implementation
+    return this.Array;
   }
 
   sort(indices) {
-    // your implementation
+    indices.sort((a, b) => (a - b));
+    for(var i = 0; i < indices.length; i++){
+      for(var j = 0; j < indices.length; j++){
+          var indexI = indices[i];
+          var indexJ = indices[j];
+          if(this.compare(this.at(indexI), this.at(indexJ)) < 0){
+              var temp = this.at(indexI);
+              this.Array[indexI] = this.at(indexJ);
+              this.Array[indexJ] = temp;
+          }
+      }
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.compare = compareFunction;
   }
 }
 
